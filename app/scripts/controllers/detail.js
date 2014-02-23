fa526905f4f02047c8e596a16d454f7254d1e59f
Edrick Ortiz <edrick.ortiz@gmail.com>
2014-02-23 17:17:07 -0400

    Merge branch 'master' of https://github.com/edxe/socia_aee
    Conflicts:
    	app/scripts/controllers/detail.js

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -1,14 +1,19 @@
 'use strict';
 
 angular.module('socialAeeApp')
-    .controller('DetailCtrl', function ($scope, $http,$log) {
-        $scope.items = [];
+    .controller('DetailCtrl', function ($scope, $http,$log,$rootScope) {
 
+        $scope.dataObj = null;
+        $scope.pueblo = $rootScope.incident.name
 //        $http({method: 'GET', url: 'http://aeebe.ngrok.com/getBreakdownsByTownOrCity/San%20Juan' })
-      /*  $http({method: 'GET', url: 'http://localhost:8002/getBreakdownsByTownOrCity/San%20Juan' })
+        $http({method: 'GET', url: 'http://localhost:8002/getBreakdownsByTownOrCity/'+$rootScope.incident.name+"/" })
             .success(function (data, status, headers, config) {
-                $log.info(data);
-                $scope.items = data;
+                $scope.doStuff(data);
 
-            });*/
+            });
+
+        $scope.doStuff = function(data) {
+            $scope.dataObj = data != null ? angular.fromJson(data) : null;
+
+        };
     });
