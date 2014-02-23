71a4f757912b16150f514cc9edb2d7504eefa043
Edrick Ortiz <edrick.ortiz@gmail.com>
2014-02-23 17:03:17 -0400

    - google reverse geolocation

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('socialAeeApp')
-    .controller('DetailCtrl', function ($scope) {
+    .controller('DetailCtrl', function ($scope, $http,$log) {
+        $scope.items = [];
 
+//        $http({method: 'GET', url: 'http://aeebe.ngrok.com/getBreakdownsByTownOrCity/San%20Juan' })
+      /*  $http({method: 'GET', url: 'http://localhost:8002/getBreakdownsByTownOrCity/San%20Juan' })
+            .success(function (data, status, headers, config) {
+                $log.info(data);
+                $scope.items = data;
+
+            });*/
     });
