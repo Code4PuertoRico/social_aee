cbda3346b544b1d4b4d79d9f1f6293cd55e6d6f2
Luis Catoni <psychoff@gmail.com>
2014-02-23 15:48:21 -0400

    main screen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('socialAeeApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, $log) {
         var pueblos = ["ADJUNTAS", "AGUADA", "AGUADILLA", "AGUAS BUENAS", "AIBONITO", "ANASCO", "ARECIBO",
             "ARROYO", "BARCELONETA", "BARRANQUITAS", "BAYAMON", "CABO ROJO", "CAGUAS", "CAMUY",
             "CANOVANAS", "CAROLINA", "CATANO", "CAYEY", "CEIBA", "CIALES", "CIDRA", "COAMO",
@@ -14,6 +14,8 @@ angular.module('socialAeeApp')
             "SAN SEBASTIAN", "SANTA ISABEL", "TOA ALTA", "TOA BAJA", "TRUJILLO ALTO", "UTUADO", "VEGA ALTA",
             "VEGA BAJA", "VIEQUES", "VILLALBA", "YABUCOA", "YAUCO"];
 
+        $scope.pueblo = null;
+
         $scope.pueblos = [];
 
         for (var i = 0; i < pueblos.length; i++ ) {
@@ -21,5 +23,27 @@ angular.module('socialAeeApp')
             $scope.pueblos.push(pueblo);
         }
 
-        $scope.pueblo = null;
+        $scope.restData = null;
+        $scope.stats = null;
+        $scope.dataObj = null;
+
+        $scope.doStuff = function(data) {
+            $scope.restData = data;
+            $scope.dataObj = data != null ? angular.fromJson(data) : null;
+            var incidents = angular.isObject($scope.dataObj) && angular.isObject($scope.dataObj.return) ? $scope.dataObj.return.length : 0;
+            $scope.stats = {
+                name: $scope.pueblo.name,
+                incidents: incidents
+            };
+        };
+
+        $scope.$watch('pueblo', function(newVal, oldVal) {
+            if (!angular.isObject($scope.pueblo)) {
+                return;
+            }
+            $http({method: 'GET', url: 'http://localhost:8002/getBreakdownsByTownOrCity/' + $scope.pueblo.name + "/ "})
+                .success(function(data, status, headers, config) {
+                    $scope.doStuff(data);
+                });
+        });
   });
